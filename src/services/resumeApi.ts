5a384707fdce72d467934d57c8ca7b4b2a9f39b6
quan.np <quan.np@tinasoft.com.vn>
2024-12-04 01:03:34 +0700

    update cv

--- src/services/resumeApi.ts
+++ src/services/resumeApi.ts
@@ -4,7 +4,7 @@ import { Resume } from '../pages/cv/types';
 
 export const fetchResume = async (resumeId) => {
     try {
-        const response = await axiosIns.get(`/resumes/${resumeId}`, { includeToken: true });
+        const response = await axiosIns.get(`/resumes/${resumeId}`);
         return response.data;
     } catch (error) {
         console.error('Error fetching resume:', error);
@@ -23,14 +23,20 @@ export const resumeGet = createAsyncThunk<any>(
 );
 
 // Create resume
-export const resumeCreate = createAsyncThunk<any>(
+export const resumeCreate = createAsyncThunk<
+    any, // Kiểu dữ liệu trả về
+    { name: string }, // Kiểu của tham số đầu vào
+    { rejectValue: string } // Kiểu của giá trị trả về khi reject
+>(
     'resume/create',
-    async (_, { rejectWithValue }) => {
+    async (payload, { rejectWithValue }) => {
         try {
-            const response = await axiosIns.post('/resumes', {}, 
+            const response = await axiosIns.post(
+                '/resumes',
+                { name: payload.name }, // Gửi tên CV trong body
                 { includeToken: true }
             );
-            
+
             return { response: response.data };
         } catch (error: any) {
             console.error('Resume create error:', error);
